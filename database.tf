9a25b826758bf61c7a00d4a973f91b193df1ed36
Daniel Furman <djfurman@gmail.com>
2017-09-04 07:25:15 -0400

    working plan for database and server, testing bucket.

--- database.tf
+++ database.tf
@@ -1,11 +1,15 @@
-resource "aws_db_instance" "death-db-mysql" {
-    allocated_storage = 10
-    engine = "mariadb"
-    engine_version = "10.0.31"
-    instance_class = "db.t2.micro"
-    name = "death-db-dev"
-    username = "death-db-user"
-    password = "Booyah123" #this is an example only and will not be used in production, we're not that slow ;)
+resource "aws_db_instance" "death_mysql" {
+  allocated_storage = 10
+  engine            = "mariadb"
+  engine_version    = "10.0.31"
+  instance_class    = "db.t2.micro"
+  name              = "DeathDbDev"
+  username          = "DeathDbUser"
+  password          = "${var.db_password}"
+
+  tags {
+    CreatedBy = "${var.created_by}"
+  }
 }
 
 # resource "aws_rds_cluster_instance" "AuroraDeathAppProduction" {
@@ -15,10 +19,12 @@ resource "aws_db_instance" "death-db-mysql" {
 #     instance_class = "db.r3.medium"
 # }
 
+
 # resource "aws_rds_cluster" "default" {
 #     cluster_identifier = "aurora-death-app-production"
 #     availability_zones = ["us-east-1c", "us-east-1e"]
 #     database_name = "death-db"
 #     master_username = "death-db-user"
-#     master_password = "Booyah123"  #this is an example only and will not be used in production, we're not that slow ;)
+#     master_password = "${var.db_password}"
 # }
+
