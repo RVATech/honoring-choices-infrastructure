f1bf1933253be782aec3eedd877c63604394dbad
Daniel Furman <djfurman@gmail.com>
2017-09-04 07:55:23 -0400

    Create S3 buckets for dev, test & prod application to hold photos for processsing.

--- bucket.tf
+++ bucket.tf
@@ -12,3 +12,36 @@ resource "aws_s3_bucket" "hcva-terraform" {
     enabled = true
   }
 }
+
+resource "aws_s3_bucket" "hcva-photos-dev" {
+  bucket = "hcva-photo-processing-dev"
+  acl    = "public"
+
+  tags {
+    Name        = "HC Photo Processing for AI"
+    Environment = "Dev"
+    CreatedBy   = "${var.created_by}"
+  }
+}
+
+resource "aws_s3_bucket" "hcva-photos-test" {
+  bucket = "hcva-photo-processing-test"
+  acl    = "public"
+
+  tags {
+    Name        = "HC Photo Processing for AI"
+    Environment = "Test"
+    CreatedBy   = "${var.created_by}"
+  }
+}
+
+resource "aws_s3_bucket" "hcva-photos-prod" {
+  bucket = "hcva-photo-processing-prod"
+  acl    = "public"
+
+  tags {
+    Name        = "HC Photo Processing for AI"
+    Environment = "Prod"
+    CreatedBy   = "${var.created_by}"
+  }
+}
